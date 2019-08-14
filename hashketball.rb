9b3b0e91fb8033e29c7a07a98549cab75667cce3
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-14 14:02:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,7 @@ def player_numbers (element)
        if team_attribute == :players
          values.each do |name|
            name.each do |first_name, name_attributes|
-             team_numbers << name 
+             team_numbers << name_attributes[:points]
            binding.pry
          end
         end
