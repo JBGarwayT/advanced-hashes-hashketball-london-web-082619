999e659b6947a70f2e61a59664eed5984ec629fe
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-14 14:37:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,7 +225,7 @@ def big_shoe_rebounds
       if team_attribute == :players
         values.each do |name|
           name.each do |first_name, name_attributes|
-            binding.pry
+            #binding.pry
             if name[name.keys.first][:shoe] > biggest_shoe
               biggest_shoe = name[name.keys.first][:shoe]
               biggest_shoe_rebounds = name[name.keys.first][:rebounds]
